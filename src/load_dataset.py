ddeece19d021fa8a8e7242fd83a4590b49999c33
Sp25-bcs-034 <imaraasim01@gmail.com>
2026-07-27 12:14:53 -0700

    Week 2: Added recursive chunking and updated dependencies

--- src/load_dataset.py
+++ src/load_dataset.py
@@ -33,3 +33,41 @@ except Exception as e:
     print(e)
 train_Df.to_csv("data/cleaned/clean_ag_news.csv" , index = False )
 #print (pd.read_csv("data/cleaned/clean_ag_news.csv"))
+# chunking 
+from langchain_text_splitters import  RecursiveCharacterTextSplitter
+splitter = RecursiveCharacterTextSplitter.from_tiktoken_encoder(chunk_size= 350 , chunk_overlap = 50 )
+page_content = train_Df["text"].tolist()
+metadata = train_Df[["label"]].to_dict(orient ="records")
+train_Df_doc = splitter.create_documents(page_content, metadatas= metadata )
+print("----------------")
+print("length of the  Chunk Doc \n")
+print (len(train_Df_doc))
+print("----------------")
+print("First  Chunk  \n")
+print (train_Df_doc[0].page_content)
+print("----------------")
+print(" First  Metadata  \n")
+print(train_Df_doc[0].metadata)
+print("----------------")
+print(" length of first chunk   \n")
+print(len(train_Df_doc[0].page_content))
+print("----------------")
+print("First  3 Chunk  \n")
+for i in range(3):
+    print("----------------")
+    print(train_Df_doc[i].page_content)
+list_text =[]
+for doc in (train_Df_doc):
+    list_text.append(doc.page_content)  
+list_metadata =[]
+for doc in  train_Df_doc:
+    list_metadata.append (doc.metadata["label"])  
+data = { 
+    "text" : list_text,
+    "label": list_metadata }    
+chunk_DF = pd.DataFrame(data)   
+print("----------------")
+print(" chunk DF   \n")
+print (chunk_DF)
+#chunk_DF.to_csv("data/chunks/chunks.csv" , index = False ) 
+
